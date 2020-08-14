70ad63cfe5de01636fcf96fd336abde52e902cbc
Gabriela Guedes <zooiv3rde@gmail.com>
2020-08-13 21:31:30 -0300

    odd

---
+++ src/pages/main/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+
+    render() {
+ 
+        return (
+            <div className="odd">
+                <strong>Odd</strong>
+                <p>Retorno sobre o que será aplicado na operação</p>
+            </div>
+        ) 
+    }
+}
